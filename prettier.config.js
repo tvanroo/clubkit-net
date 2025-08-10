ca9cf871dc5fccfb06477f7965f98601c2e09a56
Toby vanRoojen <toby@vanroojen.com>
2025-08-10 14:43:06 -0700

    Add gitignore entries and prettier config
    - Updated .gitignore with ClubKit entries: .env*, convex/.env
    - Added prettier.config.js with modern formatting rules
    - Existing eslint, tsconfig, and tailwind configs already in place

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/** @type {import('prettier').Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  useTabs: false,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  vueIndentScriptAndStyle: false,
+};
